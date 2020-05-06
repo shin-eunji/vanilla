38bcd4a91c1aa111cdecafc1434958bed02a01cb
shineunji <dmswlckato@gmail.com>
2020-05-06 18:17:48 +0900

    javascript

--- toggle/script.js
+++ toggle/script.js
@@ -1,14 +1,30 @@
-let toggleInit = (function() {
-  let status = true;
+// let toggleInit = (function() {
+//   let status = true;
 
+//   return function() {
+//     if(status) {
+//       document.querySelector('.txt').classList.add('show');
+//     } else {
+//       document.querySelector('.txt').classList.remove('show');
+//     }
+//   status = !status
+//   }
+// })();
+
+// document.querySelector('.btn-tgl').addEventListener('click', toggleInit);
+
+
+const toggleInit = (function () {
+  let status = true;
+  
   return function() {
     if(status) {
       document.querySelector('.txt').classList.add('show');
     } else {
       document.querySelector('.txt').classList.remove('show');
     }
-  status = !status
+    status = !status
   }
+  
 })();
-
 document.querySelector('.btn-tgl').addEventListener('click', toggleInit);
